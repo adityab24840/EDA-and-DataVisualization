2f29afa1b6ee3cd4b0bf546d6a772ce2fcd3f63c
Aditya N Bhatt <56029648+adityab24840@users.noreply.github.com>
2023-09-06 00:43:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,141 +1,134 @@
 import seaborn as sns
 import streamlit as st
-import numpy as np
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('Agg')
 
+# Set the page title and subheader
+st.title("ML DataSet Explorer & Data Visualization")
+st.subheader("DataSet Explorer built with Streamlit")
 
-def main():
-    """ Machine Learning DataSet Explorer & Data Visualization """
-    st.title("ML DataSet Explorer & Data Visualization")
-    st.subheader("DataSet Explorer built with Streamlit")
+# Function to select a dataset file
+def file_selector(path='./datasets'):
+    files = os.listdir(path)
+    select_file = st.selectbox("Select a file(dataset)", files)
+    return os.path.join(path, select_file)
 
-    def file_selector(path='./datasets'):
-        files = os.listdir(path)
-        select_file = st.selectbox("Select a file(dataset)", files)
-        return os.path.join(path, select_file)
+# Select a dataset file
+file = file_selector()
+st.info(f"You selected {file}")
 
-    file = file_selector()
-    st.info("You selected {}".format(file))
+# Read the selected dataset
+@st.cache  # Cache the dataset to improve performance
+def load_data(file):
+    return pd.read_csv(file)
 
-    # html_temp = '''
-    # <h1 style="text-align:center;">OR</h1>
-    # '''
-    # st.markdown(html_temp, unsafe_allow_html=True)
-    # file = st.file_uploader("Upload a Dataset", type=['csv'])
-    #
-    # st.info("You selected {}".format(file))
+df = load_data(file)
 
+# Sidebar - About and GitHub link
+st.sidebar.header("About")
+st.sidebar.info("Aditya N Bhatt")
+st.sidebar.text("231057017")
+st.sidebar.text("AI & ML")
+st.sidebar.text("MSIS")
 
-    # Read File .csv
-    df = pd.read_csv(file)
+# Add your GitHub repository link here
+github_repo = "https://github.com/adityab24840/EDA-and-DataVisualization/"
+st.sidebar.markdown(f"[GitHub Repository]({github_repo})")
 
-    # Show Dataset
-    if st.checkbox('Show Dataset'):
-        st.write(df.head())
+st.sidebar.text("Built with Streamlit")
 
-    # Show Shape of Dataset
-    if st.checkbox("Shape of Dataset"):
-        st.write(df.shape)
+# Show Dataset
+if st.checkbox('Show Dataset'):
+    st.write(df.head())
 
-    # Show DataSet Column
-    if st.checkbox("Select Columns to Show"):
-        all_columns = df.columns.tolist()
-        select_columns = st.multiselect("Select", all_columns)
-        new_df = df[select_columns]
-        st.dataframe(new_df)
+# Show Shape of Dataset
+if st.checkbox("Shape of Dataset"):
+    st.write(df.shape)
 
-    # Show Values
-    if st.button("Value Counts"):
-        st.text("Value Counts By Target/Class")
-        st.write(df.iloc[:, -1].value_counts())
+# Show DataSet Column
+if st.checkbox("Select Columns to Show"):
+    all_columns = df.columns.tolist()
+    select_columns = st.multiselect("Select", all_columns)
+    new_df = df[select_columns]
+    st.dataframe(new_df)
 
-    # Checking for null value in DataSet
-    if st.checkbox("Check for null values available in dataset"):
-        st.write(df.isnull().sum())
+# Show Values
+if st.button("Value Counts"):
+    st.text("Value Counts By Target/Class")
+    st.write(df.iloc[:, -1].value_counts())
 
-    # Show DataTypes
-    if st.button("Data Types"):
-        st.write(df.dtypes)
+# Checking for null value in DataSet
+if st.checkbox("Check for null values available in dataset"):
+    st.write(df.isnull().sum())
 
-    # Show Summary
-    if st.checkbox("Summary of Dataset"):
-        st.write(df.describe())
+# Show DataTypes
+if st.button("Data Types"):
+    st.write(df.dtypes)
 
-    # Plot and Visualization
-    st.subheader("Data Visulaization")
-    #Corelation
-    #Seaborn Plot
-    if st.checkbox("Correlation Plot[Seaborn]"):
-        st.write(sns.heatmap(df.corr(),annot=True))
+# Show Summary
+if st.checkbox("Summary of Dataset"):
+    st.write(df.describe())
+
+# Data Visualization
+st.subheader("Data Visualization")
+
+# Correlation Heatmap (Seaborn Plot)
+if st.checkbox("Correlation Plot[Seaborn]"):
+    st.write(sns.heatmap(df.corr(), annot=True))
+    st.pyplot()
+
+# Pie Chart
+if st.checkbox("Pie Plot"):
+    all_columns_names = df.columns.tolist()
+    if st.button("Generate Pie Plot"):
+        st.success("Generating A Pie Plot")
+        st.write(df.iloc[:, -1].value_counts().plot.pie(autopct="%1.1f%%"))
         st.pyplot()
 
-    # Pie Chart
-    if st.checkbox("Pie Plot"):
-        all_columns_names = df.columns.tolist()
-        if st.button("Generate Pie Plot"):
-            st.success("Generating A Pie Plot")
-            st.write(df.iloc[:,-1].value_counts().plot.pie(autopct="%1.1f%%"))
-            st.pyplot()
-
-    # Count Plot
-    if st.checkbox("Plot of Value Counts"):
-        st.text("Value Counts By Target")
-        all_columns_names = df.columns.tolist()
-        primary_col = st.selectbox("Primary Columm to GroupBy",all_columns_names)
-        selected_columns_names = st.multiselect("Select Columns",all_columns_names)
-        if st.button("Plot"):
-            st.text("Generate Plot")
-            if selected_columns_names:
-                vc_plot = df.groupby(primary_col)[selected_columns_names].count()
-            else:
-                vc_plot = df.iloc[:,-1].value_counts()
-            st.write(vc_plot.plot(kind="bar"))
-            st.pyplot()
-
-    # Customizable Plot
-    st.subheader("Customizable Plot")
+# Count Plot
+if st.checkbox("Plot of Value Counts"):
+    st.text("Value Counts By Target")
     all_columns_names = df.columns.tolist()
-    type_of_plot = st.selectbox("Select Type of Plot",["area","bar","line","hist","box","kde"])
-    selected_columns_names = st.multiselect("Select Columns To Plot",all_columns_names)
+    primary_col = st.selectbox("Primary Columm to GroupBy", all_columns_names)
+    selected_columns_names = st.multiselect("Select Columns", all_columns_names)
+    if st.button("Plot"):
+        st.text("Generate Plot")
+        if selected_columns_names:
+            vc_plot = df.groupby(primary_col)[selected_columns_names].count()
+        else:
+            vc_plot = df.iloc[:, -1].value_counts()
+        st.write(vc_plot.plot(kind="bar"))
+        st.pyplot()
 
-    if st.button("Generate Plot"):
-        st.success("Generating Customizable Plot of {} for {}".format(type_of_plot,selected_columns_names))
+# Customizable Plot
+st.subheader("Customizable Plot")
+all_columns_names = df.columns.tolist()
+type_of_plot = st.selectbox("Select Type of Plot", ["area", "bar", "line", "hist", "box", "kde"])
+selected_columns_names = st.multiselect("Select Columns To Plot", all_columns_names)
 
-        # Plot By Streamlit
-        if type_of_plot == 'area':
-            cust_data = df[selected_columns_names]
-            st.area_chart(cust_data)
+if st.button("Generate Plot"):
+    st.success(f"Generating Customizable Plot of {type_of_plot} for {selected_columns_names}")
 
-        elif type_of_plot == 'bar':
-            cust_data = df[selected_columns_names]
-            st.bar_chart(cust_data)
+    # Plot By Streamlit
+    if type_of_plot == 'area':
+        cust_data = df[selected_columns_names]
+        st.area_chart(cust_data)
 
-        elif type_of_plot == 'line':
-            cust_data = df[selected_columns_names]
-            st.line_chart(cust_data)
+    elif type_of_plot == 'bar':
+        cust_data = df[selected_columns_names]
+        st.bar_chart(cust_data)
 
-		# Custom Plot
-        elif type_of_plot:
-            cust_plot= df[selected_columns_names].plot(kind=type_of_plot)
-            st.write(cust_plot)
-            st.pyplot()
+    elif type_of_plot == 'line':
+        cust_data = df[selected_columns_names]
+        st.line_chart(cust_data)
 
-    if st.button("Thanks"):
-        st.balloons()
+    # Custom Plot
+    elif type_of_plot:
+        cust_plot = df[selected_columns_names].plot(kind=type_of_plot)
+        st.write(cust_plot)
+        st.pyplot()
 
-    
-    st.sidebar.header("About")
-    st.sidebar.info("Aditya N Bhatt")
-    st.sidebar.text("231057017")
-    st.sidebar.text("AI & ML")
-    st.sidebar.text("MSIS")
-    github_repo = "https://github.com/adityab24840/EDA-and-DataVisualization/" 
-    st.sidebar.markdown(f"[GitHub Repository]({github_repo})")	
-    st.sidebar.text("Built with Streamlit")
+# "Thanks" Button
+if st.button("Thanks"):
+    st.balloons()
 
-if __name__ == '__main__':
-    main()
